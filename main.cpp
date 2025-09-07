6b0ff24c95be699d5430cadb3f5cb7942144325e
Rudy <rudy.benavides@g.austincc.edu>
2025-09-06 23:30:26 -0500

    created a whileloop to print out parameters

--- main.cpp
+++ main.cpp
@@ -5,8 +5,15 @@
 #include "functions.h"
 
 int main(int argc, char** argv) {
-    printf("The program name is '%s'\n", argv[0]);
-    printf("The length of the program name is %d\n", strlength(argv[0]));
+    printf("The program name is '%s'\n", get_filename(argv[0]));
+    printf("The length of the program name is %d\n", strlength(get_filename(argv[0])));
     printf("The number of command line parameters: %d\n", argc - 1);
+    
+    int i = 1;
+    while (i < argc) {
+        printf("\tThe length of parameter %d is : %d\n", i, strlength(argv[i]));
+        i++;
+    }
+    
     return 0;
 }
