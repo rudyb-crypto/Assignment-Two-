0d6a4cd843021c1ab24dcd7bb8a4b705e05c2233
Rudy <rudy.benavides@g.austincc.edu>
2025-09-06 23:35:24 -0500

    created function for get_filename w/ whileloop

--- functions.cpp
+++ functions.cpp
@@ -11,6 +11,7 @@
 #endif //COMMNADLINE_FUNCTIONS_H
 
 int strlength(const char *str){
+	if (str == nullptr) return 0;
 	int length = 0;
 	while (str[length] != '\0'){
 		length++;
@@ -18,7 +19,18 @@ int strlength(const char *str){
 	return length;
 }
 
-
+const char* get_filename(const char *path) {
+    if (path == nullptr) return ""; // Return empty string for null input
+    const char *last_separator = path;
+    int i = 0;
+    while (path[i] != '\0') {
+        if (path[i] == '/' || path[i] == '\\') { // Check for both / and \ for portability
+            last_separator = path + i + 1; // Point to character after separator
+        }
+        i++;
+    }
+    return last_separator;
+}
 
 
 
